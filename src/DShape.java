f86d2292b30693b0bde7ba607b0f15c191a3488e
Izzy Moriguchi <izumimoriguchi@gmail.com>
2017-11-13 21:23:04 -0800

    Add DShape class

---
+++ src/DShape.java
@@ -0,0 +1,29 @@
+public class DShape {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+
+    public DShape(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
