61cd4cb793934ee3021aca82c7b2e56ee02ae395
Izzy Moriguchi <izumimoriguchi@gmail.com>
2017-11-13 21:24:34 -0800

    Canvas: Added background color and canvas size

--- src/Canvas.java
+++ src/Canvas.java
@@ -1,5 +1,11 @@
 import javafx.scene.layout.Pane;
 
 public class Canvas extends Pane {
+    public Canvas() {
+        super();
+        setStyle("-fx-background-color: hotpink;");
+
+        setPrefSize(400, 400);
+    }
 
 }
